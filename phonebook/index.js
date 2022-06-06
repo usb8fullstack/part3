7cbf44f355836ce060487ff65b0b8a39d848ea23
Hieu Nguyen <nth937@gmail.com>
2022-06-06 20:48:38 +0300

    ex3.3

--- phonebook/index.js
+++ phonebook/index.js
@@ -41,7 +41,51 @@ app.get('/api/info', (request, response) => {
   )
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
 
+// app.delete('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   persons = persons.filter(person => person.id !== id)
+
+//   response.status(204).end()
+// })
+
+// const generateId = () => {
+//   const maxId = persons.length > 0
+//     ? Math.max(...persons.map(n => n.id))
+//     : 0
+//   return maxId + 1
+// }
+
+// app.post('/api/persons', (request, response) => {
+//   const body = request.body
+
+//   if (!body.content) {
+//     return response.status(400).json({ 
+//       error: 'content missing' 
+//     })
+//   }
+
+//   const person = {
+//     content: body.content,
+//     important: body.important || false,
+//     date: new Date(),
+//     id: generateId(),
+//   }
+
+//   persons = persons.concat(person)
+
+//   response.json(person)
+// })
 
 
 const PORT = 3001
