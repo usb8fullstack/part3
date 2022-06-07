c1b3674f81fb606b0c43eb3dfa2a9a78b57b7de3
Hieu Nguyen <nth937@gmail.com>
2022-06-07 22:41:48 +0300

    basic query data

--- phonebook/index.js
+++ phonebook/index.js
@@ -8,14 +8,25 @@ app.use(cors())
 
 app.use(express.json())
 
+/*************************************************/
 app.use(express.static('build'))
 
 morgan.token('body', function getBody (req, res) {
   return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-// app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
-// app.use(morgan('tiny'))
+
+/*************************************************/
+const mongoose = require('mongoose')
+const url = `mongodb+srv://hp:${process.env.PASSWORD}@cluster0.2znsjnu.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+const Person = mongoose.model('Person', personSchema)
 
 let persons = [
   { 
@@ -40,12 +51,25 @@ let persons = [
   }
 ]
 
+/*************************************************/
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person
+    .find({})
+    .then(persons => {
+      response.json(persons)
+    })
 })
 
 app.get('/api/info', (request, response) => {
