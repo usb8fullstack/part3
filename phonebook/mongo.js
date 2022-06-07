f4f43ee7fd9b4b9d039f1fb9aa966c51f1153b09
Hieu Nguyen <nth937@gmail.com>
2022-06-07 21:18:44 +0300

    ex3.12

--- phonebook/mongo.js
+++ phonebook/mongo.js
@@ -1,47 +1,54 @@
-// node mongo.js <password>
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
-}
+// if (process.argv.length < 3) {
+//   console.log('Please provide the password ... as an argument: node mongo.js <password> ...')
+//   process.exit(1)
+// }
 
 const password = process.argv[2]
-
-// TODO: change link, then <phonebookApp>
+// TODO: change link, change <phonebookApp>
 const url = `mongodb+srv://hp:${password}@cluster0.2znsjnu.mongodb.net/phonebookApp?retryWrites=true&w=majority`
-
-const personSchema = new mongoose.Schema({  // TODO: change name note >> person ...
-  // content: String,
-  // date: Date,
-  // important: Boolean,
+const personSchema = new mongoose.Schema({
   name: String,
   number: String,
 })
-
 const Person = mongoose.model('Person', personSchema)
 
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log('connected')
-    Person.find({}).then(result => {
-    // Person.find({ name: "hihi"}).then(result => {
+
+if (process.argv.length > 3) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  mongoose
+    .connect(url)
+    .then(() => {
+      console.log('connected')
+      const person = new Person({
+        name: name,
+        number: number,
+      })
+      return person.save()
+    })
+    .then(() => {
+      console.log(`added ${name} number ${number} to phonebook`)
+      return mongoose.connection.close()
+    })
+    .catch((err) => console.log(err))
+}
+
+if (process.argv.length === 3) {
+  mongoose
+    .connect(url)
+    .then(() => {
+      console.log('connected')
+      return Person.find({})
+      return Person.find({ name: "hihi"})
+    })
+    .then(result => {
       result.forEach(person => {
         console.log(person)
       })
       mongoose.connection.close()
     })
-
-    // const person = new Person({
-    //   name: 'hihi',
-    //   number: '2222',
-    // })
-
-    // return person.save()
-  })
-  // .then(() => {
-  //   console.log('person saved!')
-  //   return mongoose.connection.close()
-  // })
-  .catch((err) => console.log(err))
+    .catch((err) => console.log(err))
+}
