963d10f2c82e046870514500c93d833683ea9783
Hieu Nguyen <nth937@gmail.com>
2022-06-07 17:31:00 +0300

    ex3.9 & ex3.10

---
+++ phonebook/README.md
@@ -0,0 +1,1 @@
+https://hp-phonebook.herokuapp.com/api/persons
--- phonebook/index.js
+++ phonebook/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+require('dotenv').config()
 const app = express()
+
+const cors = require('cors')
+app.use(cors())
 
 app.use(express.json())
 
@@ -107,7 +111,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
